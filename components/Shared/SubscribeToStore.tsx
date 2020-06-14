ae3c76557f873c87373ea5e869648f5f5adf58f9
Jim Cosgrave <jcosgrave@bdo.com>
2020-06-14 18:22:19 -0500

    Stubbed out unsubscribe from store api call logic

--- components/Shared/SubscribeToStore.tsx
+++ components/Shared/SubscribeToStore.tsx
@@ -3,9 +3,8 @@ import fetch from 'isomorphic-unfetch';
 import { env } from '../../util/environment';
 import MyTypeahead from './MyTypeahead';
 import { myGet } from '../../util/myGet';
-import { SUBSCRIBE_TO_STORE_API_METHOD } from '../../util/constants';
+import { SUBSCRIBE_TO_STORE_API_METHOD, UNSUBSCRIBE_FROM_STORE_API_METHOD } from '../../util/constants';
 
-//const updateGroceryUrl = env.apiUrl + 'list';
 const getStoresApiUrl = env.apiUrl + 'user?method=getStores';
 const postUserApiUrl = env.apiUrl + 'user';
 
@@ -24,6 +23,9 @@ const SubscribeToStore = (props) => {
         if (!offline) {
             execute();
         } else {
+            //
+            // Offline mode - hard code some test data
+            //
             setSubbedStores([
                 { _id: '123abc', name: 'Aldi (Lemont IL)' },
                 { _id: '456def', name: 'Jewel (Lemont IL)' }
@@ -38,6 +40,10 @@ const SubscribeToStore = (props) => {
         const clone = subbedStores.slice();
         const existing = clone.find(s => s._id.toString() == store.id);
 
+        //
+        // Only proceed if the user chose a store they're not currently
+        // subscribed to
+        //
         if (!existing) {
             if (!offline) {
                 const body = {
@@ -54,14 +60,27 @@ const SubscribeToStore = (props) => {
                     body: JSON.stringify(body)
                 });
 
+                //
+                // Call the API
+                //
                 const json = await resp.json();
 
+                //
+                // Update state
+                //
                 setSubbedStores(json.stores);
 
+                //
+                // If the parent component wants to respond to this event
+                // propigate the event upward.
+                //
                 if(typeof(props.onSubscribe) === 'function') {
                     props.onSubscribe(json.stores[0]);
                 }
             } else {
+                //
+                // Offline mode, is client side only
+                //
                 const newStore = { _id: store.id, name: store.label };
 
                 clone.push(newStore);
@@ -70,12 +89,38 @@ const SubscribeToStore = (props) => {
         }
     }
 
-    function handleUnsubscribeClick(store) {
+    //
+    // Handle the event when a user unsubscribes from a store
+    //
+    async function handleUnsubscribeClick(store) {
+        //
+        // Find the existing store and remove it from the stores array
+        //
         let clone = subbedStores.slice();
         const index = clone.indexOf(store);
 
         clone.splice(index, 1);
         setSubbedStores(clone);
+
+        if(!offline) {
+            //
+            // If we have internet connection, send the request to the server
+            //
+            const body = {
+                method: UNSUBSCRIBE_FROM_STORE_API_METHOD,
+                store_id: store._id.toString()
+            };
+
+            const resp = await fetch(postUserApiUrl, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(body)
+            });
+
+            const json = await resp.json();
+        }
     }
 
     function getHTML() {
