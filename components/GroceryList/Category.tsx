feb91478c3b8c9f16cb0a61b4d8c8479aaa80f12
Jim Cosgrave <jcosgrave@bdo.com>
2020-07-11 12:50:50 -0500

    Added new Category component

---
+++ components/GroceryList/Category.tsx
@@ -0,0 +1,16 @@
+import React, { useState, useRef, useEffect } from 'react';
+import fetch from 'isomorphic-unfetch';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckSquare } from '@fortawesome/free-solid-svg-icons';
+import { faSquare } from '@fortawesome/free-regular-svg-icons';
+import { env } from '../../util/environment';
+
+const Category = (props) => {
+    return (
+        <div>
+            New Category Component
+        </div>
+    );
+};
+
+export default Category;
