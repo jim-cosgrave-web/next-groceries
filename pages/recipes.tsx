3c2bb300714e497e1c6ae84681569b9be4e1aa2e
Jim Cosgrave <jcosgrave@bdo.com>
2020-07-20 21:25:35 -0500

    Added recipe search feature

--- pages/recipes.tsx
+++ pages/recipes.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import Link from 'next/link';
 import { env } from '../util/environment';
 import { myGet } from '../util/myGet';
@@ -11,8 +11,11 @@ const apiUrl = env.apiUrl + 'recipes';
 
 const RecipesPage = () => {
     const [recipes, setRecipes] = useState(null);
+    const [visibleRecipes, setVisibleRecipes] = useState(null);
+    const searchRef = useRef<HTMLInputElement>(null);
 
     useEffect(() => {
+        searchRef.current.value = '';
         let isCancelled = false;
 
         async function execute() {
@@ -21,6 +24,7 @@ const RecipesPage = () => {
             if (isCancelled == false) {
                 data.recipes.sort(compare);
                 setRecipes(data.recipes);
+                setVisibleRecipes(data.recipes);
             }
         }
 
@@ -34,10 +38,10 @@ const RecipesPage = () => {
     function getLinkClass(r) {
         let c = "item item-hoverable clickable";
 
-        if(r.clicked) {
+        if (r.clicked) {
             c += " item-clicked";
         }
-        
+
         return c;
     }
 
@@ -46,20 +50,59 @@ const RecipesPage = () => {
         const clone = recipes.slice();
         const index = recipes.indexOf(r);
 
-        for(let i = 0; i < clone.length; i++) {
-            if(i != index) {
+        for (let i = 0; i < clone.length; i++) {
+            if (i != index) {
                 clone[i].clicked = false;
             } else {
                 clone[i].clicked = true;
             }
         }
-        
+
         setRecipes(clone);
+    }
+
+    function handleNewClick() {
+        Router.replace('/recipes/create');
+    }
+
+    function handleSearch() {
+        let searchTerm = searchRef?.current?.value;
+
+        if (!searchTerm || searchTerm.length == 0) {
+            setVisibleRecipes(recipes);
+            return;
+        }
+
+        const clone = recipes.slice();
+        searchTerm = searchTerm.trim().toLowerCase();
+
+        let visible = [];
+
+        for (let i = 0; i < clone.length; i++) {
+            let recipe = clone[i];
+
+            if (recipe.name.trim().toLowerCase().indexOf(searchTerm) > -1) {
+                visible.push(recipe);
+            } else {
+                for (let j = 0; j < recipe.categories.length; j++) {
+                    if (recipe.categories[j].trim().toLowerCase().indexOf(searchTerm) > -1) {
+                        visible.push(recipe);
+                        break;
+                    }
+                }
+            }
+        }
+
+        setVisibleRecipes(visible);
     }
 
     function getJSX() {
         if (!recipes) {
             return <div>Loading...</div>;
+        }
+
+        if (!visibleRecipes || visibleRecipes.length == 0) {
+            return <div>Nothing found...</div>;
         }
 
         if (recipes && recipes.length == 0) {
@@ -75,21 +118,29 @@ const RecipesPage = () => {
 
         let jsx = null;
 
-        jsx = recipes.map((r, index) => {
+        jsx = visibleRecipes.map((r, index) => {
             return (
                 <Link key={r._id} href={`/recipes/${r._id.toString()}`}>
-                    <div onClick={() => handleRecipeClick(r)} className={'item item-hoverable clickable' + (r.clicked ? ' item-clicked' : '')} key={r.name}>
-                        {r.name}
+                    <div className={'item item-hoverable clickable' + (r.clicked ? ' item-clicked' : '')}>
+                        
+                            <div onClick={() => handleRecipeClick(r)} key={r.name}>
+                                {r.name}
+                            </div>
+                            <div className="flex mt-10">
+                            {r.categories.map((c, cIndex) => {
+                                return (
+                                    <div className="mr-10 tag" key={c}>
+                                        {c}
+                                    </div>
+                                );
+                            })}
+                        </div>
                     </div>
                 </Link>
             );
         });
 
         return jsx
-    }
-
-    function handleNewClick() {
-        Router.replace('/recipes/create');
     }
 
     return (
@@ -101,6 +152,9 @@ const RecipesPage = () => {
                 <div className="clickable" onClick={handleNewClick}>
                     <FontAwesomeIcon icon={faPlus} />
                 </div>
+            </div>
+            <div className="mb-10">
+                <input ref={searchRef} type="text" className="form-control" placeholder="Search by name or keywords" onKeyUp={handleSearch} />
             </div>
             <div className="list">
                 {getJSX()}
