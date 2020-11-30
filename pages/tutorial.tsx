730a30136eababd5734ddb54248509633d235634
Jim Cosgrave <jcosgrave@bdo.com>
2020-11-29 18:46:58 -0600

    Starated tutorial page

--- pages/tutorial.tsx
+++ pages/tutorial.tsx
@@ -5,8 +5,20 @@ const TutorialPage = () => {
 
     return (
         <div>
-            <h1>Tutorial Page</h1>
+            <h1>Groceries Tutorial</h1>
+            <div className="tutorial-wrapper">
+                <div className="tutorial-section">
+                    <div className="tutorial-title">
+                        Getting Started
+                    </div>
+                    <div className="tutorial-text">
 
+                    </div>
+                    <div>
+                        <img src="/images/tutorial/groceries-tutorial-01.png" />
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
