741306c7252508c48481a86a7737bf1f609f49c2
Jim Cosgrave <jcosgrave@bdo.com>
2020-12-26 21:19:44 -0600

    Change max height on AddToHome

--- components/Shared/AddToHome.tsx
+++ components/Shared/AddToHome.tsx
@@ -10,7 +10,8 @@ const customStyles = {
         marginRight: '-50%',
         transform: 'translate(-50%, -50%)',
         padding: '0',
-        maxHeight: '700px',
+        maxHeight: '75vh',
+        maxWidth: '85vw',
         overflow: 'hidden',
         border: '0',
         borderRadius: '14px'
@@ -22,14 +23,17 @@ const customStyles = {
 
 const headerStyles = {
     padding: '1em',
-    border: '1px solid #000'
+    border: '1px solid #000',
+    fontSize: '22px',
+    backgroundColor: '#21313a',
+    color: '#fff'
 }
 
 const bodyStyles = {
     borderLeft: '1px solid #000',
     borderRight: '1px solid #000',
     overflow: 'auto',
-    maxHeight: '500px'
+    maxHeight: '400px'
 }
 
 const footerStyles = {
@@ -40,23 +44,41 @@ const footerStyles = {
 Modal.setAppElement('#__next');
 
 const AddToHome = (props) => {
+    const [modalIsOpen, setIsOpen] = useState(true);
+
+    function handleGotItClick() {
+        setIsOpen(false);
+    }
+
     return (
         <div>
             <Modal
                 style={customStyles}
                 contentLabel="Add to Home Screen"
-                isOpen={true}
+                isOpen={modalIsOpen}
             >
                 <div id="add-to-home-header" style={headerStyles}>
                     Add to Home Screen
                 </div>
                 <div id="add-to-home-body" style={bodyStyles}>
+                    <div style={{
+                        padding: '1em'
+                    }}>
+                        Follow these steps to add the Groceries app to your home screen for easy access.
+                    </div>
                     <div>
                         <img src="/images/tutorial/add-to-home-iphone.png" />
                     </div>
                 </div>
                 <div id="add-to-home-footer" style={footerStyles}>
-                    Footer
+
+                    <button className="clickable my-button w-100"
+                        onClick={handleGotItClick}
+                        style={{
+                            backgroundColor: '#54925e'
+                        }}>
+                        Got it!
+                    </button>
                 </div>
             </Modal>
         </div>
